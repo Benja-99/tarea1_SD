2d3bb8070e2f22fb9af49dbd978fc52e44489be9
root <root@DESKTOP-M5IIVB2.localdomain>
2021-11-03 23:06:38 -0300

    Se arreglo el Pozo

--- client/client.go
+++ client/client.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"log"
 
 	"example.com/m/chat/github.com/Benja-99/tarea1_SD/chat"
+	"example.com/m/pozo/github.com/Benja-99/tarea1_SD/pozo"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
-func client_bot(conn *grpc.ClientConn){
+func client_bot(conn *grpc.ClientConn) {
 	c := chat.NewChatServiceClient(conn)
+	c_pozo := pozo.NewPozoServiceClient(conn)
 	var response *chat.Message
 	var err error
 	response, err = c.Peticion(context.Background(), &chat.Message{Body: "Quiero jugar"})
- 	if err != nil {
-    	log.Fatalf("Error when calling Peticion: %s", err)
-  	}
-  	log.Printf("Response from server: %s", response.Body)
-	if response.Body == "Jugador ingresado"{
-		
+	if err != nil {
+		log.Fatalf("Error when calling Peticion: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Body)
+	if response.Body == "Jugador ingresado" {
+		var response *pozo.Monto
+		response, err = c_pozo.GetMonto(context.Background(), &pozo.Monto{CantidadTotal: 1})
+		if err != nil {
+			log.Fatalf("Error when calling Peticion: %s", err)
+		}
+		log.Printf("Response from server: %d", response.CantidadTotal)
 	}
 }
 
@@ -33,7 +40,7 @@ func main() {
 	defer conn.Close()
 	for i := 1; i <= 15; i++ {
 		go client_bot(conn)
-	} 
-	
+	}
+
 	fmt.Scanln()
 }
