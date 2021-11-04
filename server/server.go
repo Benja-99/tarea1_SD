75bb0a1468cbefa542ee64a53bf313a0b8c2c965
root <root@DESKTOP-M5IIVB2.localdomain>
2021-11-03 22:56:04 -0300

    xd

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	"example.com/m/chat/github.com/Benja-99/tarea1_SD/chat"
+	"example.com/m/pozo/github.com/Benja-99/tarea1_SD/pozo"
 	"google.golang.org/grpc"
 )
 
@@ -20,9 +21,12 @@ func main() {
 
 	s := chat.Server{}
 
+	s_pozo := pozo.Server{}
+
 	grpcServer := grpc.NewServer()
 
 	chat.RegisterChatServiceServer(grpcServer, &s)
+	pozo.RegisterPozoServiceServer(grpcServer, &s_pozo)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
